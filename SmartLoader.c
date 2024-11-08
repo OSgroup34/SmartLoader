40e2c321296ee2a85e283fa2442aae1bc0fb9022
GunikaSingh <gunika505@gmail.com>
2024-11-09 02:20:41 +0530

    done?

--- SmartLoader.c
+++ SmartLoader.c
@@ -11,70 +11,46 @@
 
 Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
-Elf32_Phdr **phdr_arr;
 int fd;
 int pagefaults=0;
+int numOfPages=0;
+int mappedSize;
+Elf32_Phdr *mappedPhdr;
+int PAGESIZE=4096;
+size_t fragmentation;
+void* pageAllocated;
+void* mappedPages[100];
+char* heapmemalloc;
+//assuming max pages required 100
 
-typedef struct mapped_phdr {
-    Elf32_Phdr* m_phdr;
-    int size;} 
-
-mapped_phdr;
-mapped_pdhr **delete_arr;
-int no_phdr;
-
-void sigsegvHandler(int signo, siginfo_t *info, void *context){
-    if (signo==SIGSEGV){
-        void* addr=(*info).si_addr;
-        pagefaults++;
-        for(int i=0;i<(*ehdr).e_phnum;i++){
-
-        }
-    }
-}
-
-void free_mapped_phdr(mapped_phdr* phdr){
-    if(phdr){
-        if (munmap((*phdr).m_phdr,(*phdr).size)==-1){
-            perror("Error: munmap");
-            exit(0);
-        }
-        free(phdr);
-}}
-void clean_up(){
-    free(ehdr);
-    for(int i=0; i<no_phdr; i++){
-        free(phdr_arr[i];)
-    }
-    free(phdr_arr);
-
-    if(delete_arr){
-        for(int i=0; i<no_phdr;i++){
-            free_mapped_phdr(delete_arr[i]);
-        }
-        free(delete_arr);
-}}
-
-int main(int argc,char** argv){
-    if(argc!=2){
-        printf("Incorrect or missing arguments");
+void loadAndRunElf(char** exe){
+    fd=open(*exe, O_RDONLY);
+    off_t size=lseek(fd,0,SEEK_END);
+    //error handling
+    if (size==-1){
+        printf("Error in getting file size\n");
         exit(1);
     }
-    //error handling for ELF file
-    fd=open(argv[1], O_RDONLY);
-    if(fd==-1){ 
-        perror("Error while opening");
-        exit(0);
-    }
-    int e_size=sizeof(Elf32_Ehdr);
-    ehdr=(Elf32_Ehdr*)malloc(e_size*sizeof(char));                
-    if(ehdr==NULL){ 
-        perror("Error allocating malloc");
-        exit(0);
-    }
     lseek(fd,0,SEEK_SET);
-    read(fd,ehdr,e_size*sizeof(char));
-    //checking magic numbers
+    //entire file memory allocation
+    heapmemalloc=(char*)malloc(size);
+    //error handling
+    if (!heapmemalloc){
+        printf("error in memory allocation\n");
+        exit(1);
+    }
+    ssize_t readfile=read(fd, heapmemalloc,size);
+
+    //error handling
+    if (readfile<0 || (size_t)readfile!=size){
+    perror("Error in reading file");
+    free(heapmemalloc);
+    exit(1);
+    }
+
+    ehdr=(Elf32_Ehdr*)heapmemalloc;
+    phdr=(Elf32_Phdr*)(heapmemalloc+(*ehdr).e_phoff);
+
     int x;
     if(!ehdr)
         x=1;
@@ -82,16 +58,65 @@ int main(int argc,char** argv){
         x=1;    
     if(x==1){
         printf("Invalid ELF file.\n");
-        free(ehdr);
-        exit(0);
-    }
-    free(ehdr);
-    int close_error=close(fd);
-    if (close_error==-1){
-        perror("Error in closing elf");
+        cleanup();
         exit(0);
     }
 
+    int (*_start)(void) = (int (*)(void))(*ehdr).e_entry;
+    int result = _start();
+    printf("User _start return value = %d\n", result);
+    printf("Total page faults: %d\n",pagefaults);
+    printf("Pages Allocated: %d\n",numOfPages);
+    printf("Total fragmentation (in KB): %0.4f bytes\n",fragmentation);
+    }
+
+
+void sigsegvHandler(int signo, siginfo_t *info, void *context){
+    if (signo==SIGSEGV){
+        pagefaults++;
+        void* addr=(*info).si_addr;
+        size_t pageStart=((size_t)addr/PAGESIZE)*PAGESIZE;
+        int segment=-1;
+        for(int i=0;i<(*ehdr).e_phnum;i++){
+            if ((*(phdr+i)).p_type==PT_LOAD && (*(phdr + i)).p_vaddr<=addr && ((*(phdr + i)).p_vaddr+(*(phdr + i)).p_memsz)>=info->si_addr){
+                segment=i;
+                break;
+            }
+        }
+        if (segment==-1){
+            printf("fault not occured in given segments.");
+            exit(1);
+        }
+        void *pageAddr=(void *)((*(phdr+segment)).p_vaddr+numOfPages*PAGESIZE);
+        pageAllocated=mmap(pageAddr,PAGESIZE, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_FIXED, fd,((*(phdr+segment)).p_offset+numOfPages*PAGESIZE));
+        mappedPages[numOfPages]=pageAllocated;
+        numOfPages++;
+        if (pageAllocated == MAP_FAILED){
+        printf("mmap error");
+        exit(1);}
+        fragmentation=PAGESIZE-(*(phdr+segment)).p_memsz;
+    }
+}
+
+void cleanup(){
+    free(heapmemalloc);
+    if(mappedPages){
+        for(int i=0; i<numOfPages;i++){
+            if (munmap(mappedPages[i],PAGESIZE)==-1){
+            perror("munmap error");
+            exit(0);
+        }}}
+    if (close(fd)==-1){
+        perror("Error in closing elf");
+        exit(0);
+    }
+}
+
+int main(int argc,char** argv){
+    if(argc!=2){
+        printf("Incorrect or missing arguments");
+        exit(1);
+    }
     struct sigaction sig;
     memset(&sig,0,sizeof(sig));
     sig.sa_sigaction=sigsegvHandler;
@@ -101,7 +126,7 @@ int main(int argc,char** argv){
         exit(0);
     }
 
-    load_and_run_elf(&argv[1]);
-    clean_up();
+    loadAndRunElf(&argv[1]);
+    cleanup();
     return 0;
 }
